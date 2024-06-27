1bf9036447e31803b00085612eb1c9e0d99cf70f
Koterlan David <koterlandavid@gmail.com>
2024-06-27 17:45:31 +0200

    Update host_website.sh to use the "aws_region" terraform output and construct the URL inside the script

--- host_website.sh
+++ host_website.sh
@@ -3,8 +3,8 @@
 # Extract bucket name and URL from Terraform outputs
 cd ./infrastructure || exit
 BUCKET_NAME="$(terraform output -raw bucket_name)"
-URL="$(terraform output -raw s3_bucket_url)"
 PROFILE="$(terraform output -raw aws_profile)"
+REGION="$(terraform output -raw aws_region)"
 
 # Check if the bucket name was successfully retrieved
 if [ -z "$BUCKET_NAME" ]; then
@@ -12,15 +12,15 @@ if [ -z "$BUCKET_NAME" ]; then
     exit 1
 fi
 
-# Check if the URL was successfully retrieved
-if [ -z "$URL" ]; then
-    echo "Error: S3 bucket URL not found in Terraform output."
-    exit 1
-fi
-
 # Check if the AWS profile was successfully retrieved
 if [ -z "$PROFILE" ]; then
     echo "Error: AWS profile not found in Terraform output."
+    exit 1
+fi
+
+# Check if the AWS region was successfully retrieved
+if [ -z "$REGION" ]; then
+    echo "Error: AWS region not found in Terraform output."
     exit 1
 fi
 
@@ -31,4 +31,4 @@ aws s3api put-object --profile "$PROFILE" --bucket "$BUCKET_NAME" --key style.cs
 aws s3api put-object --profile "$PROFILE" --bucket "$BUCKET_NAME" --key cat_image.png --body "./app/cat_image.png" --content-type "image/png"
 
 echo "You have posted a static webpage with the following URL: "
-echo "$URL"
+echo "https://$BUCKET_NAME.s3.$REGION.amazonaws.com/index.html"
